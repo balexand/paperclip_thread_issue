f170289667785208f8286a08d576c1da8383474e
balexand <balexand@gmail.com>
2014-11-22 13:18:32 -0700

    log path before and after call

--- app/workers/create_image_worker.rb
+++ app/workers/create_image_worker.rb
@@ -3,6 +3,9 @@ class CreateImageWorker
   sidekiq_options retry: false
 
   def perform
+    Rails.logger.info "***PATH before: #{ENV['PATH']}"
     Image.create! image: File.new(Rails.root + 'test/fixtures/example.jpg')
+  ensure
+    Rails.logger.info"***PATH after: #{ENV['PATH']}"
   end
 end
